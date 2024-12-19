ed0b4adda2ead853652c749fe4ce855278b215d7
hammadsarwar786 <hammadarain326@gmail.com>
2024-12-19 10:08:18 +0500

    url change

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 export const constant = {
 
-    baseUrl: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:5000/" : "https://server.idxdubai.com/",
+    baseUrl: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:5000/" : "https://wmtest.idxdubai.com/",
     server2: "https://testing.idxdubai.com/"
     //  baseUrl: (!process.env.NODE_ENV || process.env.NODE_ENV === 'https://crm-stage.weeam.info/') ? "http://localhost:5000/" : "https://server.idxdubai.com/",
     // server2: "https://testing.idxdubai.com/"
